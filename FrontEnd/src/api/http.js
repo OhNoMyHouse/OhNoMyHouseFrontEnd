3867010d74a5aeb7a52cf1b3d94b7f740cb0f1ff
정태영 <jty419@naver.com>
2022-05-23 21:00:23 +0900

    TY 0523 Modify store class -> sotre module

--- FrontEnd/src/api/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:8080/myapp_rest",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
--- FrontEnd/src/common/Constant.js
+++
@@ -1,9 +0,0 @@
-export default {
-  GET_QNAS: "getQnas",
-  GET_QNA: "getQna",
-  REGIST_QNA: "registQna",
-  MODIFY_QNA: "modifyQna",
-  DELETE_QNA: "deleteQna",
-  SET_QNAS: "setQnas",
-  SET_QNA: "setQna",
-};
