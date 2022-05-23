9217f9f6f90170dc14a44cf2d6299cc6b011c6cb
joo-jj <70085233+joo-jj@users.noreply.github.com>
2022-05-23 21:48:48 +0900

    0523 JJ houselistitem.vue

--- components/House/HouseListItem.vue
+++ components/House/HouseListItem.vue
@@ -1,11 +1,17 @@
 <template>
-  <b-row
-    class="m-2"
-    @click="selectHouse"
-    @mouseover="colorChange(true)"
-    @mouseout="colorChange(false)"
-    :class="{ 'mouse-over-bgcolor': isColor }"
-  >
+  <b-row class="m-2" @click="selectHouse" @mouseover="colorChange(true)" @mouseout="colorChange(false)" :class="{ 'mouse-over-bgcolor': isColor }">
+    <b-card img-src="@/assets/img/house.png" img-alt="Image" img-top tag="article" style="max-width: 20rem" class="mb-2">
+      <b-card-text>
+        <strong>{{ house.aptName }}</strong>
+      </b-card-text>
+      <b-card-text>
+        <b-list-group flush>
+          <b-list-group-item>{{ house.sidoName }} {{ house.gugunName }} {{ house.dongName }}</b-list-group-item>
+          <b-list-group-item>실거래가 : {{ house.recentPrice }} 만원</b-list-group-item>
+        </b-list-group>
+      </b-card-text>
+    </b-card>
+    <div></div>
   </b-row>
 </template>
 
